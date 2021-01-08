4f0f0e72d334ea1d60d03527600cca3c74f2a7cd
Jaehoon So <jhspacelover@naver.com>
2021-01-09 01:09:31 +0900

    baekjoon

---
+++ 1008.cpp
@@ -0,0 +1,12 @@
+// A/B
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout.precision(10);
+    long double a, b;
+    cin >> a >> b;
+    cout << (long double)a / b;
+
+    return 0;
+}
