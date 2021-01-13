e90d48f43bd3e31e85470091e7ae986c5a765d92
CowJaehoon <76734067+Jaehoon-So@users.noreply.github.com>
2021-01-14 02:36:51 +0900

    DP

--- 9095_123더하기.cpp
+++ 9095_123더하기.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 #define MAX 100000
-int D[MAX];
+int D[MAX]; // D[i]: 
 void initialize(){
     for(int i = 0; i < MAX; i++){
         D[i] = 0;
@@ -9,9 +9,7 @@ void initialize(){
     D[1] = 1;
     D[2] = 2;
     D[3] = 4;
-    // 문제예시에 따라 하면 절대로 이러한 갯수가 나오지 않는데
-    // 이러한 갯수를 정해주어야만 올바르게 실행되는것을 보아 출제자가
-    // 잘못 한 것 같다.
+
 }
 int main(){
     int t;
